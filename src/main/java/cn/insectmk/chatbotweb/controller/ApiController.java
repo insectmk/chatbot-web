a5923d5afbabab1699f6f1ce2732f8619d72525e
makuning <makun_ing@126.com>
2024-03-05 17:54:59 +0800

    增加API的流式对话

--- src/main/java/cn/insectmk/chatbotweb/controller/ApiController.java
+++ src/main/java/cn/insectmk/chatbotweb/controller/ApiController.java
@@ -1,14 +1,15 @@
 package cn.insectmk.chatbotweb.controller;
 
 import cn.insectmk.chatbotweb.common.Result;
-import cn.insectmk.chatbotweb.controller.dto.ChatMessageDto;
+import cn.insectmk.chatbotweb.configure.SseEmitterUTF8;
+import cn.insectmk.chatbotweb.entity.ChatMessage;
 import cn.insectmk.chatbotweb.exception.BizException;
 import cn.insectmk.chatbotweb.service.ChatMessageService;
 import cn.insectmk.chatbotweb.service.ChatSessionService;
 import cn.insectmk.chatbotweb.util.AESUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.util.Objects;
 
 /**
@@ -26,6 +27,27 @@ public class ApiController {
     private ChatSessionService chatSessionService;
     @Autowired
     private AESUtil aesUtil;
+
+    /**
+     * 发送消息
+     * @param key
+     * @param chatMessage
+     * @return
+     */
+    @PostMapping("/send/stream")
+    public SseEmitter sendStream(String key, @RequestBody ChatMessage chatMessage) {
+        String sessionId = aesUtil.decrypt(key.replaceAll(" ", "+"));
+        // 判断会话是否存在
+        if (Objects.isNull(chatSessionService.getById(sessionId))) {
+            throw new BizException("该接口已失效");
+        }
+        // 发送消息
+        chatMessage.setSessionId(sessionId);
+        SseEmitterUTF8 sseEmitterUTF8 = new SseEmitterUTF8(-1L);
+        chatMessageService.sendStream(chatMessage, sseEmitterUTF8);
+
+        return sseEmitterUTF8;
+    }
 
     /**
      * 查询会话所有的历史消息
@@ -46,19 +68,19 @@ public class ApiController {
     /**
      * 发送消息
      * @param key
-     * @param chatMessageDto
+     * @param chatMessage
      * @return
      */
     @PostMapping("/send")
-    public Result send(String key, @RequestBody ChatMessageDto chatMessageDto) {
+    public Result send(String key, @RequestBody ChatMessage chatMessage) {
         String sessionId = aesUtil.decrypt(key.replaceAll(" ", "+"));
         // 判断会话是否存在
         if (Objects.isNull(chatSessionService.getById(sessionId))) {
             throw new BizException("该接口已失效");
         }
 
-        chatMessageDto.setSessionId(sessionId);
+        chatMessage.setSessionId(sessionId);
 
-        return Result.buildSuccess(chatMessageService.send(chatMessageDto));
+        return Result.buildSuccess(chatMessageService.send(chatMessage));
     }
 }
