99e19d6f282af92ef1569fe71cbdc5dfc920efd1
makuning <makun_ing@126.com>
2024-04-19 13:10:13 +0800

    将API文档接口由写死的方式改为读取文件的方式

--- src/main/java/cn/insectmk/chatbotweb/controller/ApiController.java
+++ src/main/java/cn/insectmk/chatbotweb/controller/ApiController.java
@@ -9,9 +9,14 @@ import cn.insectmk.chatbotweb.exception.BizException;
 import cn.insectmk.chatbotweb.service.ChatMessageService;
 import cn.insectmk.chatbotweb.service.ChatSessionService;
 import cn.insectmk.chatbotweb.util.AESUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 /**
@@ -23,6 +28,7 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/api")
 @RequestLimit(maxCount = 1,second = 5)
+@Slf4j
 public class ApiController {
     @Autowired
     private ChatMessageService chatMessageService;
@@ -39,41 +45,36 @@ public class ApiController {
      */
     @GetMapping("/tips")
     public Result getApiTips() {
-        StringBuilder markdown = new StringBuilder();
-
-        markdown.append("## 发送消息\n\n");
-        markdown.append("### 普通对话\n\n");
-        markdown.append("- 接口地址：`");
-        markdown.append(systemValue.getUrl());
-        markdown.append("/api/send?key={你的API密钥}`\n");
-        markdown.append("- 请求类型：`POST`\n");
-        markdown.append("- 请求数据格式：\n");
-        markdown.append("  ```json\n");
-        markdown.append("  {\n");
-        markdown.append("      \"messageContent\": \"你好\"\n");
-        markdown.append("  }\n");
-        markdown.append("  ```\n\n");
-        markdown.append("### 流式对话\n\n");
-        markdown.append("- 接口地址：`");
-        markdown.append(systemValue.getUrl());
-        markdown.append("/api/send/stream?key={你的API密钥}`\n");
-        markdown.append("- 请求类型：`POST`\n");
-        markdown.append("- 请求数据格式：\n");
-        markdown.append("  ```json\n");
-        markdown.append("  {\n");
-        markdown.append("      \"messageContent\": \"你好\"\n");
-        markdown.append("  }\n");
-        markdown.append("  ```\n\n");
-        markdown.append("## 对话历史\n\n");
-        markdown.append("- 接口地址：`");
-        markdown.append(systemValue.getUrl());
-        markdown.append("/api?key={你的API密钥}`\n");
-        markdown.append("- 请求类型：`GET`\n\n");
-        markdown.append("## 注意\n\n");
-        markdown.append("1. 每位用户只能同时拥有一个API密钥\n");
-        markdown.append("2. 重新生成API密钥会导致旧密钥的失效，并且会清除所有对话内容\n");
-
-        return Result.buildSuccess(markdown);
+        // 获取API说明文档输入流
+        InputStream tipsStream = getClass().getResourceAsStream("/template/api-tips.md");
+        if (tipsStream == null) {
+            throw new BizException("API说明文件未找到！");
+        }
+        // 创建拼接
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(tipsStream));
+        String line;
+        // 读取文件
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+            // 将url替换为项目地址并返回
+            while (sb.indexOf("{{url}}") != -1) {
+                sb.replace(sb.indexOf("{{url}}"), sb.indexOf("{{url}}") + "{{url}}".length(), systemValue.getUrl());
+            }
+            return Result.buildSuccess(sb);
+        } catch (IOException e) {
+            log.error("API文档读取错误：", e);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                log.error("API文档读取错误：", e);
+            }
+        }
+        return Result.buildSuccess();
     }
 
     /**
