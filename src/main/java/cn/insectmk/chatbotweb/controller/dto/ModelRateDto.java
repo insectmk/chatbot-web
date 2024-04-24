2a47100fc791e044df118f1df130497bedd4e660
makuning <makun_ing@126.com>
2024-04-24 14:06:05 +0800

    增加模型评分的添加功能

---
+++ src/main/java/cn/insectmk/chatbotweb/controller/dto/ModelRateDto.java
@@ -0,0 +1,16 @@
+package cn.insectmk.chatbotweb.controller.dto;
+
+import cn.insectmk.chatbotweb.entity.ModelRate;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Description 模型评分传输对象
+ * @Author makun
+ * @Date 2024/4/24 13:36
+ * @Version 1.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ModelRateDto extends ModelRate {
+}
