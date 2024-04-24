3fbfbaa8c35f7aa2a8e4878fafd27f3089be0fc7
makuning <makun_ing@126.com>
2024-04-24 17:59:42 +0800

    增加更新对话消息的接口

--- src/main/java/cn/insectmk/chatbotweb/controller/ChatMessageController.java
+++ src/main/java/cn/insectmk/chatbotweb/controller/ChatMessageController.java
@@ -2,13 +2,11 @@ package cn.insectmk.chatbotweb.controller;
 
 import cn.insectmk.chatbotweb.common.Result;
 import cn.insectmk.chatbotweb.common.annotation.RequestLimit;
+import cn.insectmk.chatbotweb.controller.dto.ChatMessageDto;
 import cn.insectmk.chatbotweb.entity.ChatMessage;
 import cn.insectmk.chatbotweb.service.ChatMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 /**
@@ -25,15 +23,27 @@ public class ChatMessageController {
     private ChatMessageService chatMessageService;
 
     /**
+     * 更新聊天消息的信息（点赞功能）
+     * @param chatMessageDto
+     * @return
+     */
+    @PutMapping
+    public Result edit(@RequestBody ChatMessageDto chatMessageDto) {
+        return chatMessageService.updateById(chatMessageDto) ?
+                Result.buildSuccess() :
+                Result.buildFail();
+    }
+
+    /**
      * 发送消息（流式输出）
-     * @param chatMessage
+     * @param chatMessageDto
      * @return
      */
     @RequestLimit(maxCount = 1000,second = 1)
     @PostMapping("/stream")
-    public SseEmitter sendStream(@RequestBody ChatMessage chatMessage) {
+    public SseEmitter sendStream(@RequestBody ChatMessageDto chatMessageDto) {
         SseEmitter sseEmitter = new SseEmitter(-1L);
-        chatMessageService.sendStream(chatMessage, sseEmitter);
+        chatMessageService.sendStream(chatMessageDto, sseEmitter);
         return sseEmitter;
     }
 
--- src/main/java/cn/insectmk/chatbotweb/entity/ChatMessage.java
+++ src/main/java/cn/insectmk/chatbotweb/entity/ChatMessage.java
@@ -19,13 +19,16 @@ public class ChatMessage {
     public static final String SENDER_TYPE_USER = "user";
     public static final String SENDER_TYPE_SYSTEM = "system";
 
+    public static final int IS_LIKE_NONE = 0; // 无感
+    public static final int IS_LIKE_YES = 1; // 喜欢
+    public static final int IS_LIKE_NO = 2; // 不喜欢
+
     protected String id;
     protected String sessionId;
     protected String senderType;
     protected String messageId;
     protected String messageContent;
-    protected Boolean isLike;
-    protected Boolean isNotLike;
+    protected Integer isLike;
     protected Long wordCount;
     protected Long tokens;
     protected String modelVersionId;
